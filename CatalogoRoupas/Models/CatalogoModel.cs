fc37ffbf77afb2af16a8c3509eb7fd069d020d13
Augusto Monteiro <augustoengenharia@outlook.com>
2023-05-22 10:39:11 -0300

    Adicionar arquivos de projeto.

---
+++ CatalogoRoupas/Models/CatalogoModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CatalogoRoupas.Models
+{
+    [Table("Catalogo")]
+    public class CatalogoModel
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Marca")]
+        [Display(Name = "Marca")]
+        public string Marca { get; set; }
+
+        [Column("Masculino")]
+        [Display(Name = "Sexo")]
+        public bool Marculino { get; set; }
+
+        [Column("Peca")]
+        [Display(Name = "Peça")]
+        public string Peca { get; set; }
+    }
+}
